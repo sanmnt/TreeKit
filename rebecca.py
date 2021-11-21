0532020831ea41683bbb4bdb23bd842c5225d087
rlee56800 <78502232+rlee56800@users.noreply.github.com>
2021-11-21 01:26:38 -0800

    initial commit

---
+++ rebecca.py
@@ -0,0 +1,34 @@
+from playsound import playsound
+import time
+
+### LET THERE BE LIGHTS
+def lightJP():
+    # Lights "John Python" in Morse code
+    # .---/---/..../-.//.--./-.--/-/..../---/-.
+    print()
+
+def blink():
+    # If the lights turning off is too harsh, change it in here
+    print()
+
+
+### AN AUDIO EXPERIENCE
+### download playsound (pip install playsound==1.2.2) to play sound
+def chaos():
+    pickme = []
+    print()
+    # randomly play one:
+    #   toad sings
+    #   rick roll?
+
+def funTimes():
+    playsound("fun.mp3")
+    print("Dancing Queen")
+
+def calm():
+    print("Claire de Lune")
+    time.sleep(1)
+    print("Claro")
+
+def festivity():
+    print("Carol of the Bells")
